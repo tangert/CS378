2b08d68f72278857442762b51bb600ac4b80ef5b
tangert <tyler@angert.com>
2018-02-18 00:47:56 -0500

    complete

--- hw1/apriori.py
+++ hw1/apriori.py
@@ -1,40 +1,44 @@
+# THIS CODE IS MY OWN WORK AND WAS WRITTEN WITHOUT CONSULTING A TUTOR OR CODE WRITTEN BY OTHER STUDENTS
+# -TYLER ANGERT
 
 from __future__ import division
 from collections import Counter
 import itertools as it
 import time
-import math
 import sys
 
-# globals
-DATA_FILE_PATH = "T10I4D100K.dat"
-MIN_SUPPORT = 500
-TEST_LIMIT = 99999
+# global for storing candidates
+ALL_CANDIDATES = []
 
 ##########################################
 # MARK: File conversion and output methods
 ##########################################
 
 def convert(filename):
-    """
-    Converts every line (transaction) in the DAT file into a 2D array
-    """
+    # converts every line in the dat file into a huge 2D array
     data = []
+    for line in open("{}".format(filename), 'r'):
+        data.append(line.rstrip().split(" "))
+    return data
 
-    for line in open(filename, 'r'):
-        if len(data) < TEST_LIMIT:
-            data.append(line.rstrip().split(" "))
+def save_output(candidates, file_name):
+
+    output = open(file_name,"w+")
+
+    for i in range(0, len(candidates)):
+        if i > 0:
+            for item in sorted(candidates[i]):
+                output_str = ""
+
+                for subitem in item:
+                    output_str += "{} ".format(subitem)
+
+                print output_str
+                output.write("{} ({})\n".format(output_str, candidates[i][item]))
         else:
-            return data
-
-    # for line in open(filename, 'r'):
-    #     data.append(line.rstrip().split(" "))
-    #
-    # return data
-
-
-def save_output():
-    print "output"
+            for item in sorted(candidates[i]):
+                print item
+                output.write("{} ({})\n".format(item, candidates[i][item]))
 
 
 ######################################
@@ -43,29 +47,49 @@ def save_output():
 
 ########
 def create_single_frequent_itemset(transactions, min_support):
-    """
-     special method to handle creating a single itemset at first
-
-     1. gets the item counts
-     2. count how many times it shows up for each transaction
-     3. if that count is above the min_support, add it to the frequent itemset
-    """
 
     item_counts = Counter()
 
     for transaction in transactions:
         item_counts.update(transaction)
 
-    frequent_item_set = set(item for item in item_counts
+    frequents = Counter(item for item in item_counts
                             if item_counts[item] >= min_support)
-    return frequent_item_set
 
-#######
+    for item in frequents:
+        frequents[item] = item_counts[item]
+
+    return frequents
+
+def create_double_frequent_itemset(transactions, min_support):
+
+    item_counts = Counter()
+
+    # go through each transaction
+    # count all of the combinations of two from each transaction as you go
+
+    for transaction_group in transactions:
+        for transaction in transaction_group:
+            combos = set(it.combinations(transaction,2))
+            for combo in combos:
+                item_counts.update([combo])
+
+    frequents = Counter(item for item in item_counts
+                        if item_counts[item] >= min_support)
+
+    for item in frequents:
+        frequents[item] = item_counts[item]
+
+    return frequents
+
 # Multi item set functions
 def filter_transactions(transactions, current_candidate, first_candidate=True):
     """
     returns a new reference database to check support counts for
     remove transaction if all items have been discarded
+    #removes elements from each transaction that are not in the itemset at all
+    #sorts the transactions by length, then groups them into groups of other
+    transactions by that length for easy indexing
     """
     filtered_transactions = []
     current_items = set()
@@ -74,77 +98,132 @@ def filter_transactions(transactions, current_candidate, first_candidate=True):
         for subset in current_candidate:
             current_items = current_items.union(subset)
     else:
-        current_items = current_candidate
+        current_items = set(current_candidate)
 
-    for transaction in transactions:
-        all_found = True
-        for item in transaction:
-            if item not in current_items and all_found:
-                all_found = False
-        if all_found:
-            # print "transaction's items all in current items!: {}".format(transaction)
-            filtered_transactions.append(transaction)
+    if first_candidate:
+        for transaction in transactions:
+                at_least_one_found = False
 
-    print "Filtered length: {}".format(len(filtered_transactions))
+                for item in transaction:
 
-    return filtered_transactions
+                    if item in current_items:
+                        at_least_one_found = True
+                        break
 
+                if at_least_one_found:
+                    filtered = list(filter(lambda x: x in current_candidate, transaction))
+                    filtered_transactions.append(filtered)
 
-def get_supported_itemset(current_candidate, supports, min_support):
+    else:
+        # Once past the first group, everything is sorted into different length transactions
+        for transaction_group in transactions:
+            for transaction in transaction_group:
+
+                at_least_one_found = False
+
+                for item in transaction:
+                    if item in current_items:
+                        at_least_one_found = True
+                        break
+
+                if at_least_one_found:
+                    filtered = list(filter(lambda x: x in current_candidate, transaction))
+                    filtered_transactions.append(filtered)
+
+    # sort them by length
+    filtered_transactions = sorted(filtered_transactions, key=len)
+
+    # split into subarrays also by length
+    sublist = []
+    total_transaction_list = []
+
+    # now the transactions are sorted and arranged into groups by actual transaction length
+    for i in range(0,len(filtered_transactions)-1):
+        if len(filtered_transactions[i]) == len(filtered_transactions[i+1]):
+            sublist.append(filtered_transactions[i])
+        else:
+            sublist.append(filtered_transactions[i])
+            total_transaction_list.append(sublist)
+            sublist = []
+
+    return total_transaction_list
+
+def get_supported_itemset(supports, min_support):
     """"
     returns the supported item set based on the counter
     """
     itemset = set()
 
-    for item in current_candidate:
-        if supports[str(item)] >= min_support:
+    for item in supports:
+        if supports[item] >= min_support:
             itemset.add(item)
 
     return itemset
 
 
-def get_itemset_supports(transactions, itemset, candidate_count):
+def get_itemset_supports(transactions, itemset, candidate_count, min_support):
 
     """
     returns a counter filled with all items and their support counts
     """
-    print "\nCALCULATING SUPPORT FOR: {} items in {} TRANSACTIONS".format(len(itemset), len(transactions))
-    supports = {}
 
-    # for item in itemset:
-    #     supports[str(item)] = 0
+    #dont check supersets of items with infrequent items
+    #these combos are all made from frequent singles tho
 
-    support2 = Counter()
+    support_counter = Counter()
+    start_time = time.time()
 
-    #current O(transactions * items)
-    #way too slow
+    # keep counts for individual elements
+    subitem_counter = Counter()
 
-    for trans in transactions:
+    print "start time support counter: {}".format(start_time)
+    print "Itemset: {}".format(itemset)
 
-        # print trans
-        print "current support len: {}\n".format(len(supports))
+    # infrequent_items = []
+    # store infrequent items as you go
+    # if the next item contains either element ? no
 
-        for item in itemset:
+    infrequent_item_transactions = {}
+    itemset = sorted(itemset)
 
-            all_found = True
+    #obviously you dont have to check every single combination
+    #so if you go through all of the transactions for an item and find which item causes it to be infrequent
 
-            # single items
-            if candidate_count < 2:
-                if item not in trans:
-                    all_found = False
+    for item in itemset:
+
+        # initialize how many transactions ahve been found
+        trans_count = 1
+
+        # go through each of the transaction groups
+        # each group represents an increasing length of the transactions inside, e.g.:
+        # transactions[0] = all transactions of length 1
+        # transactions[1] = all transactions of length 2, etc
+        # this allows you to easily skip over entire groups of transactions that are shorter in length than the items you are checking
+
+        for transaction_group_index in range(0, len(transactions)):
+
+            if len(item) <= transaction_group_index+1:
+                for trans in transactions[transaction_group_index]:
+
+                        trans_count += 1
+
+                        # assume all sub_elements found
+                        all_found = True
+
+                        for subitem in item:
+                            if subitem not in trans:
+                                all_found = False
+                                break
+
+                        #went through each item and found everything
+                        if all_found:
+                            support_counter.update([item])
             else:
-                # double items
-                for sub_item in item:
-                    if sub_item not in trans and all_found:
-                        all_found = False
+                continue
 
-            if all_found:
-                if str(item) not in supports:
-                    supports[str(item)] = 0
-                else:
-                    supports[str(item)] += 1
+    print "end time support counter: {}".format(time.time()-start_time)
 
-    return supports
+    return support_counter
 
 #####################################
 # MARK: Primary apriori set functions
@@ -165,40 +244,36 @@ def self_join(itemset, candidate_count):
 
     """
 
-    if candidate_count < 3:
-        combos = set(it.combinations(itemset, 2))
-        # for combo in combos:
-        #     print combo
-        return combos
-    else:
-        new_items = []
-        seen_items = {}
+    new_items = []
+    seen_items = {}
 
-        #used to avoid unnecessary unions when both items have already been seen
-        print "Itemset length for self join: {}".format(len(itemset))
-        for item in itemset:
-            seen_items[item] = False
+    # used to avoid unnecessary unions when both items have already been seen
+    for item in itemset:
+        seen_items[item] = False
 
-        for a in itemset:
+    for a in itemset:
 
-            # mark the item as seen
-            seen_items[a] = True
+        # mark the item as seen
+        seen_items[a] = True
 
-            for b in itemset:
+        for b in itemset:
 
-                # check if the k-2 elements are the same to
-                # check joining compatibility
+            # check if the k-2 elements are the same to
+            # check joining compatibility
 
-                if a[:candidate_count - 2] == b[:candidate_count - 2] and a != b:
+            if a[:candidate_count - 2] == b[:candidate_count - 2] and a != b:
 
-                    if seen_items[b] is False:
+                if seen_items[b] is False:
 
-                        # only form the union if item b hasn't been seen before
-                        union = set(a).union(set(b))
-                        new_items.append(tuple(union))
-                        seen_items[b] = True
+                    # only form the union if item b hasn't been seen before
+                    union = set(a).union(set(b))
 
-        return set(new_items)
+                    #only add the union if it meets the support criteria?
+
+                    new_items.append(tuple(union))
+                    seen_items[b] = True
+
+    return set(new_items)
 
 
 def prune(current_itemset, previous_itemset):
@@ -241,12 +316,15 @@ def generate_candidate_set(transactions, min_support, itemset, candidate_count):
     pruned = prune(joined, itemset)
     print "PRUNED LENGTH: {}".format(len(pruned))
 
-    supports = get_itemset_supports(transactions, pruned, candidate_count)
-    print "supports: {}".format(supports)
-    support_set = get_supported_itemset(pruned, supports, min_support)
-    print "SUPPORTED LENGTH: {} || SET: {}".format(len(support_set), support_set)
+    supports = get_itemset_supports(transactions, pruned, candidate_count, min_support)
+    print "SUPPORTS: {}".format(supports)
 
-    return support_set
+    ALL_CANDIDATES.append(supports)
+
+    supported_itemset = get_supported_itemset(supports, min_support)
+    print "SUPPORTED SET: {}".format(supported_itemset)
+
+    return supported_itemset
 
 
 def apriori(transactions, min_support):
@@ -263,15 +341,14 @@ def apriori(transactions, min_support):
     print "INITIAL TRANSACTION DB LENGTH: {}".format(len(transactions))
 
     current_candidate = create_single_frequent_itemset(transactions, min_support)
+    ALL_CANDIDATES.append(current_candidate)
+
+    final_candidate = current_candidate
     candidate_count = 1
     transactions = filter_transactions(transactions, current_candidate, first_candidate=True)
 
-    print "CURRENT TRANSACTIONS LENGTH: {}".format(len(transactions))
-
-    # filter the db references
-    # new_db = filter_transactions(data, current_candidate)
-
-    final_candidate = set()
+    current_candidate = create_double_frequent_itemset(transactions, min_support)
+    ALL_CANDIDATES.append(current_candidate)
 
     while len(current_candidate) != 0:
 
@@ -280,16 +357,11 @@ def apriori(transactions, min_support):
 
         print "\nCANDIDATE NUMBER: {}".format(candidate_count)
 
-        #store the final candidate before it gets updated
-        final_candidate = current_candidate
-
         # generate the current candidate based on the new count
         current_candidate = generate_candidate_set(transactions, min_support, current_candidate, candidate_count)
         transactions = filter_transactions(transactions, current_candidate,first_candidate=False)
 
-        print "CURRENT TRANSACTIONS LENGTH: {}".format(len(transactions))
-
-    return final_candidate
+        print "CURRENT TRANSACTIONS LENGTH: {}".format(sum([len(l) for l in transactions]))
 
 #####################################
 # MARK: MAIN
@@ -298,20 +370,21 @@ def apriori(transactions, min_support):
 if __name__ == '__main__':
 
     # command line arguments
-    # INPUT_DATA = sys.argv[0]
-    # MIN_SUPPORT = sys.argv[1]
-    # OUTPUT_FILE = sys.argv[2]
+    INPUT_DATA = sys.argv[1]
+    MIN_SUPPORT = int(sys.argv[2])
+    OUTPUT_FILE = sys.argv[3]
 
     # turn the transactions into a 2D array
-    transactions = convert(DATA_FILE_PATH)
+    transactions = convert(INPUT_DATA)
 
     # measure the start time
     start_time = time.time()
 
     # run the algorithm
-    candidate = apriori(transactions, MIN_SUPPORT)
+    apriori(transactions, MIN_SUPPORT)
 
-    print "\nFINAL CANDIDATE: {}".format(candidate)
+    # save all of the candidates into a file
+    save_output(ALL_CANDIDATES, OUTPUT_FILE)
 
     # done!
     print("--- %s seconds ---" % (time.time() - start_time))
