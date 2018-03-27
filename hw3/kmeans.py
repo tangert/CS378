bab08b59663417e21a622e616cecea0811125f39
tangert <tyler@angert.com>
2018-03-27 15:41:14 -0400

    fixed empty row bug

--- hw3/kmeans.py
+++ hw3/kmeans.py
@@ -4,8 +4,11 @@ import math
 import sys
 
 
-# Calculates the euclidean distance between any two n-dimensional points
 def calc_distance(point1, point2):
+
+    """
+    Calculates the euclidean distance between any two n-dimensional points
+    """
 
     # groups all of the dimension values together from the points
     zipped = zip(point1, point2)
@@ -24,7 +27,11 @@ def calc_distance(point1, point2):
     return total_dist
 
 
-def calc_euclidean_average(data_points):
+def calc_average(data_points):
+
+    """
+     Calculates the average of an array of n-dimensional data points
+    """
 
     sums = {}
     avgs = {}
@@ -43,6 +50,9 @@ def calc_euclidean_average(data_points):
 
 
 def calc_sse(centroids, data):
+    """
+     Calculates the sum of squared distances between data and centroids
+    """
 
     # stores each cluster's squared sums
     cluster_difs = {}
@@ -63,15 +73,28 @@ def calc_sse(centroids, data):
         # add the squared dist to the cluster's sum
         cluster_difs[centroid.cluster] += pow(distance, 2)
 
-    sse = sum(cluster_difs.values())
-
-    return sse
+    return sum(cluster_difs.values())
 
 
 def k_means(k, data):
 
+    """
+     Runs the k-means clustering algorithm on a given dataset
+    """
+
     # 1.a. initialize the clusters with k random locations
-    centroids = [Centroid(random.choice(data), i) for i in range(k)]
+    chosen = []
+    centroids = []
+
+    # assures that you pick unique random points
+    for i in range(k):
+        d = random.choice(data)
+        if d not in chosen:
+            chosen.append(d)
+            c = Centroid(d, i)
+            centroids.append(c)
+
+    print "iInitial centroids: {}".format([c.data for c in centroids])
 
     # 1.b. convert the raw data in data point objects that track their current/previous centroid
     data_points = [DataPoint(point) for point in data]
@@ -90,9 +113,11 @@ def k_means(k, data):
             min_dist = sys.float_info.max
             closest_centroid = None
 
+            # print "all centroids: {}".format([c.data for c in centroids])
+
             for centroid in centroids:
                 dist = calc_distance(point.data, centroid.data)
-                if dist <= min_dist:
+                if dist < min_dist:
                     min_dist = dist
                     closest_centroid = centroid
 
@@ -100,12 +125,16 @@ def k_means(k, data):
             point.previous_centroid = point.current_centroid
             point.current_centroid = closest_centroid
 
-            if point.current_centroid is not point.previous_centroid:
+            if point.previous_centroid is None:
                 all_centroids_same = False
+            else:
+                if point.current_centroid is not point.previous_centroid \
+                        and point.current_centroid.data != point.previous_centroid.data:
+                    all_centroids_same = False
 
         # If you get past all of the cases and each centroid is the same as the previous, you have converged
         if all_centroids_same:
-            print "All centroids same! Converging!"
+            print "Iteration {} : all centroids same! Converging!".format(iterations)
             break
 
         print "Iteration {} : calculating new centroids".format(iterations)
@@ -113,7 +142,7 @@ def k_means(k, data):
         # 2. Calculate new centroid locations
         for centroid in centroids:
             current_point_data = [point.data for point in data_points if point.current_centroid is centroid]
-            new_location = calc_euclidean_average(current_point_data)
+            new_location = calc_average(current_point_data)
             centroid.data = new_location
 
         iterations += 1
@@ -136,8 +165,11 @@ if __name__ == '__main__':
     # Remove the label from each data
     unlabeled_data = [row[:LABEL_LOCATION] for row in data]
 
-    # Converts each string data element into a float
-    numerized_data = [[float(i) for i in row] for row in unlabeled_data]
+    # Converts each string data element into a float, removes any empty rows
+    numerized_data = filter(lambda x: len(x) > 0, [[float(i) for i in row] for row in unlabeled_data])
+
+    # store sse's for post analyses
+    print "\nTESTING ON K = {}".format(INPUT_K)
 
     # Run k means on the numerized data
     centroids, results, iterations = k_means(INPUT_K, numerized_data)
@@ -148,8 +180,40 @@ if __name__ == '__main__':
     # save the output
     save_output(predicted_labels, OUTPUT_FILE)
 
-    print "\nall centroids:"
+    # convenience print statements
+    print "\nFinal centroids:"
+
     for centroid in [centroid.data for centroid in centroids]:
         print centroid
-    print "\nSSE: ", calc_sse(centroids, results)
+
+    sse = calc_sse(centroids, results)
+
+    print "\nSSE: ", sse
     print "total iterations: ", iterations
+
+    # FOR TESTING ON DIFFERENT VARIATIONS OF K
+    # for i in range(2,6):
+    #
+    #     print "\n\nTESTING ON K = {}".format(i)
+    #
+    #     # Run k means on the numerized data
+    #     centroids, results, iterations = k_means(i, numerized_data)
+    #
+    #     # Grab the labels in order
+    #     predicted_labels = [r.current_centroid.cluster for r in results]
+    #
+    #     # save the output
+    #     save_output(predicted_labels, OUTPUT_FILE)
+    #
+    #     # convenience print statements
+    #     print "\nFinal centroids:"
+    #
+    #     for centroid in [centroid.data for centroid in centroids]:
+    #         print centroid
+    #
+    #     sse = calc_sse(centroids, results)
+    #
+    #     sses[i] = sse
+    #
+    #     print "\nSSE: ", sse
+    #     print "total iterations: ", iterations
